47f941011069db803b1bc3546172198b8252db6c
Elijah <ejsisgreat@gmail.com>
2019-10-31 17:33:02 -0500

    Finish up attach styles

--- tests/unit/attach-styles.test.js
+++ tests/unit/attach-styles.test.js
@@ -2,7 +2,7 @@ const attachStyles = require("../../lib/attach-styles");
 const createMock = require("../helpers/mock-string-to-head");
 
 describe(`attachStyles`, () => {
-	it(`it should call 'string-to-head' with the correct params`, () => {
+	it(`should call 'string-to-head' with the correct params`, () => {
 		const mockFunc = jest.fn(() => {});
 		createMock(mockFunc);
 
@@ -18,5 +18,14 @@ describe(`attachStyles`, () => {
 		expect(mockFunc.mock.calls.length).toBe(1);
 		expect(mockFunc.mock.calls[0][0]).toBe(`.class{color:red}`);
 		expect(mockFunc.mock.calls[0][1]).toBe(`some-id`);
+	});
+
+	it(`should throw an error when invalid params are passed`, () => {
+		createMock(() => {});
+
+		expect(() => attachStyles("", "h")).toThrow();
+		expect(() => attachStyles(undefined, "h")).toThrow();
+		expect(() => attachStyles({})).toThrow();
+		expect(() => attachStyles({}, "")).toThrow();
 	});
 });
