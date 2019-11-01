77ad8b7e82c6aab5f59aa212fa957cff927b43ff
Elijah <ejsisgreat@gmail.com>
2019-11-01 15:07:37 -0500

    Switch out randomize for simple-random This is because simple-random is simpler

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const createStyles = require("./lib/create-styles");
-const random = require("randomatic");
+const generate = require("simple-random/browser");
 
 module.exports = {
 	parseJS: require("./lib/parsejs"),
 	attachStyles: require("./lib/attach-styles"),
 	createScope: () => {
-		const key = `s${random("aA0", 10)}`;
+		const key = `s${generate()}`;
 		const style = (obj) => createStyles(obj, key);
 
 		return { style };
